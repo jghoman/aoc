8ee89c59d3a26c5d64bfaa806916d8ca9a500100
Jakob Homan <jghoman@gmail.com>
2023-12-01 12:32:29 -0800

    Still setting up.

--- rust/day01/src/bin/part1.rs
+++ rust/day01/src/bin/part1.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("part1!");
+    let input = include_str!("./input1.txt");
+    let output = part1(input);
+    dbg!(output);
+}
+
+
+fn part1(input: &str) -> String {
+    "4".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = part1("howdy");
+        assert_eq!(result, "4".to_string());
+    }
 }
