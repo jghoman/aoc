9bcdb138454c1e90d02df535c284ed6e68c47cfc
Jakob Homan <jghoman@gmail.com>
2023-12-07 22:13:13 -0800

    Day 4, part 2 done.  Lots to refactor, but moving on.

--- 2023/rust/day04/src/bin/part1.rs
+++ 2023/rust/day04/src/bin/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 
 use nom::{
     bytes::complete::tag,
@@ -10,8 +10,12 @@ use nom::{
 
 fn main() {
     let input = include_str!("./input1.txt");
+
     let output = part1(input);
     dbg!(output);
+
+    let output2 = part2(input);
+    dbg!(output2);
 }
 
 #[derive(Debug)]
@@ -19,6 +23,12 @@ struct Scratchcard {
     card_num: u32,
     winning_numbers: Vec<u32>,
     held_numbers: Vec<u32>,
+}
+
+#[derive(Clone, Debug)]
+struct CardAndCount {
+    card_num: u32,
+    count: u32,
 }
 
 fn scratchcard(input: &str) -> IResult<&str, Scratchcard> {
@@ -67,6 +77,70 @@ fn part1(input: &str) -> String {
         .to_string()
 }
 
+fn matches_with_card_num(s: Scratchcard) -> (u32, Vec<u32>) {
+    let held: HashSet<u32> = s.held_numbers.iter().cloned().collect();
+    let winning: HashSet<u32> = s.winning_numbers.iter().cloned().collect();
+
+    (s.card_num, held.intersection(&winning).cloned().collect())
+}
+
+fn copies_from_cards(
+    cards: &Vec<CardAndCount>,
+    mymap: &BTreeMap<u32, CardAndCount>,
+) -> Vec<CardAndCount> {
+    let mut new_cards: Vec<CardAndCount> = Vec::new();
+
+    for card in cards {
+        let num = card.card_num;
+        let count = card.count;
+        let start = num + 1;
+        let end = start + count;
+
+        for i in start..end {
+            let value = mymap.get(&i).expect("Should be there").clone();
+            new_cards.push(value);
+        }
+    }
+
+    new_cards
+}
+
+fn part2(input: &str) -> String {
+    let (_, scratchcards) = parse_scratchcards(input).expect("Should parse");
+
+    let card_count_map: BTreeMap<u32, CardAndCount> = scratchcards
+        .into_iter()
+        .map(matches_with_card_num)
+        //.inspect(|p| println!("{:?}", p))
+        .map(|p| {
+            (
+                p.0,
+                CardAndCount {
+                    card_num: p.0,
+                    count: p.1.len() as u32,
+                },
+            )
+        })
+        .collect();
+
+    let mut collated: Vec<Vec<CardAndCount>> = Vec::new();
+
+    let values: Vec<CardAndCount> = card_count_map.values().cloned().collect();
+
+    let mut result = values;
+    loop {
+        collated.push(result.clone());
+        let new_results = copies_from_cards(&result, &card_count_map);
+
+        if new_results.len() == 0 {
+            break;
+        }
+        result = new_results;
+    }
+
+    collated.iter().flatten().count().to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -82,5 +156,18 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
             .to_string();
 
         assert_eq!("13", part1(&input))
+    }
+
+    #[test]
+    fn part2_works() {
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11"
+            .to_string();
+
+        assert_eq!("30", part2(&input))
     }
 }
